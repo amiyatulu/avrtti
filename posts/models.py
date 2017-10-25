5e270658353a6952b1b7afb6db634deca49a98d6
Amiya Behera <amiyatulu@gmail.com>
2017-10-25 19:12:23 +0530

    models

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,5 @@
 from django.db import models
-
-
-class Posts(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    create_time = models.DateTimeField(auto_now=True)
-    update_time = models.DateTimeField(auto_now_add=True)
-    post_hash = models.ForeignKey(PostHash, on_delete=models.CASCADE)
-
+from taggit.managers import TaggableManager
 
 class PostHash(models.Model):
     describe_change = models.TextField()
@@ -15,16 +7,24 @@ class PostHash(models.Model):
     create_time = models.DateTimeField(auto_now=True)
     update_time = models.DateTimeField(auto_now_add=True)
 
-class ReviewPosts(models.Model):
-    posts = models.OneToOneField(Posts, one_delete=models.CASCADE)
+class Posts(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     create_time = models.DateTimeField(auto_now=True)
     update_time = models.DateTimeField(auto_now_add=True)
-    review_hash = models.ForeignKey(ReviewHash, on_delete=models.CASCADE)
+    post_hash = models.ForeignKey(PostHash, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
 class ReviewHash(models.Model):
     describe_change = models.TextField()
     hashcode = models.CharField(max_length=30)
     create_time = models.DateTimeField(auto_now=True)
     update_time = models.DateTimeField(auto_now_add=True)
+
+class ReviewPosts(models.Model):
+    posts = models.OneToOneField(Posts, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    create_time = models.DateTimeField(auto_now=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+    review_hash = models.ForeignKey(ReviewHash, on_delete=models.CASCADE)
